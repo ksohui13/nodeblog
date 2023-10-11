158d02acb1577693b3e971c47d7d6776e0f00c98
ksohui13 <sohui0103@naver.com>
2023-10-11 16:19:32 +0900

    multer를 이용한 파일 업로드 구현

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const path = require("path");
 
 //.env 연결
 dotenv.config();
 //json형태로 데이터 전달
 app.use(express.json()); 
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 //mongoose
 mongoose.connect(process.env.MONGO_URL, {
@@ -19,6 +22,24 @@ mongoose.connect(process.env.MONGO_URL, {
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
+
+  
+  //파일 업로드시 저장될 위치 지정
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, "이름지정.jpeg")
+    },
+  });
+
+  //업로드
+  const upload = multer({ storage: storage });
+  app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("파일이 업로드 되었습니다.");
+  });
+
 
   //url설정
   app.use("/api/auth", authRoute);
